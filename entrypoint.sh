2f7228157b3445ee4d90b7a3326f9734eef23d3f
dIzztHreesIxty <87385723+dIzztHreesIxty@users.noreply.github.com>
2022-01-03 18:20:02 +0200

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+
+# init
+PASSWORD=${PASSWORD:-"obfspasswd"}
+METHOD=${METHOD:-"chacha20-ietf"}
+PORT=${PORT:-443}
+OBFS_TYPE=${OBFS_TYPE:-"tls"}
+
+
+# print configs
+echo "password: $PASSWORD"
+echo "method: $METHOD"
+echo "port: $PORT"
+echo "obfs type: $OBFS_TYPE"
+
+
+# modify config files
+sed -i "s/obfspasswd/$PASSWORD/g" /etc/shadowsocks-libev/config.json
+sed -i "s/http/$OBFS_TYPE/g" /etc/shadowsocks-libev/config.json
+sed -i "s/chacha20-ietf-poly1305/$METHOD/g" /etc/shadowsocks-libev/config.json
+sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
+rm /etc/nginx/sites-enabled/default
+
+
+# print configs
+cat /etc/shadowsocks-libev/config.json
+cat /etc/nginx/conf.d/default.conf
+
+
+# start nginx
+nginx
+
+
+# start obfs server
+ss-server -c /etc/shadowsocks-libev/config.json
